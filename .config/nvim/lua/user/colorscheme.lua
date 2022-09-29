b9f323f39147653c3d8e54ff5ffc8e055b47c1dd
Tyler Fisher <tfisher21@gmail.com>
2022-09-29 09:51:51 -0500

    Minor refactor to colorschemes setup

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,27 +1,22 @@
 
 -- folke/tokyonight.nvim --
--- local colorscheme = "tokyonight"
--- vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
 --
 
 -- Mofiqul/dracula.nvim --
--- local colorscheme = "dracula"
--- local dracula = require(colorscheme)
--- dracula.setup({ italic_comment = true })
+local dracula = require("dracula")
+dracula.setup({ italic_comment = true })
 --
 
 -- catppuccin/nvim --
--- local colorscheme = "catppuccin"
--- vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
--- local catppuccin = require(colorscheme)
--- catppuccin.setup({ term_colors = true })
+local catppuccin = require("catppuccin")
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+catppuccin.setup({ term_colors = true })
 --
 
--- rebelot/kanagawa.nvim --
-local colorscheme = "kanagawa"
-
 -- set colorscheme
+local colorscheme = "tokyonight" -- { "tokyonight", "dracula", "catppuccin", "kanagawa" }
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
