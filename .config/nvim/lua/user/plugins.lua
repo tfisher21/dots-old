bab8ec4d46f009767938619a6c94bac99dea3a9c
Tyler Fisher <tfisher21@gmail.com>
2023-02-17 12:44:43 -0600

    Finish migration

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -40,16 +40,16 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  use "wbthomason/packer.nvim" --use-package inspired plugin/package management for Neovim 
+  use "wbthomason/packer.nvim" --use-package inspired plugin/package management for Neovim
   use {
-   "nvim-treesitter/nvim-treesitter", -- provide a simple and easy way to use the interface for tree-sitter in Neovim 
-   run = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter", -- provide a simple and easy way to use the interface for tree-sitter in Neovim
+    run = ":TSUpdate",
   }
-  use "nvim-lua/popup.nvim" --An implementation of the Popup API from vim in Neovim 
-  use "nvim-lua/plenary.nvim" --All the lua functions I don't want to write twice 
+  use "nvim-lua/popup.nvim" --An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" --All the lua functions I don't want to write twice
   use {
     "kyazdani42/nvim-tree.lua", -- A File Explorer For Neovim Written In Lua
-    requires = {'kyazdani42/nvim-web-devicons'}, -- optional for icons
+    requires = { 'kyazdani42/nvim-web-devicons' }, -- optional for icons
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
   use "windwp/nvim-autopairs" -- A super powerful autopair plugin for Neovim that supports multiple characters.
@@ -66,18 +66,19 @@ return packer.startup(function(use)
   use "hrsh7th/nvim-cmp" --A completion engine plugin for neovim written in Lua
   use "hrsh7th/cmp-buffer" -- nvim-cmp source for buffer words
   use "hrsh7th/cmp-path" -- nvim-cmp source for filesystem paths
-  use "hrsh7th/cmp-cmdline" --nvim-cmp source for vim's cmdline 
-  use "saadparwaiz1/cmp_luasnip" --luasnip completion source for nvim-cmp 
+  use "hrsh7th/cmp-cmdline" --nvim-cmp source for vim's cmdline
+  use "saadparwaiz1/cmp_luasnip" --luasnip completion source for nvim-cmp
   use "hrsh7th/cmp-nvim-lsp" -- nvim-cmp source for neovim's built-in language server client
   use "hrsh7th/cmp-nvim-lua" -- nvim-cmp source for neovim Lua API.
 
   -- snippets --
   use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" --Snippets collection for a set of different programming languages for faster development 
+  use "rafamadriz/friendly-snippets" --Snippets collection for a set of different programming languages for faster development
 
   -- LSP --
-  use "neovim/nvim-lspconfig" -- Configurations for Nvim LuaSnip
-  use "williamboman/nvim-lsp-installer" -- plugin that allow you to manage LSP servers (NOTE: This has been deprecated and recommends migrating to mason.nvim)
+  use 'williamboman/mason.nvim' -- Portable package manager for Neovim that runs everywhere Neovim runs.
+  use "neovim/nvim-lspconfig" -- Configs for the Nvim LSP client
+  use 'williamboman/mason-lspconfig.nvim' -- mason-lspconfig bridges mason.nvim with the lspconfig plugin
   use "jose-elias-alvarez/null-ls.nvim" -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua.
 
   -- Notetaking --
